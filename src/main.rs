6cfbc6c1f1de5a88774ec20e1199aec553dae6a9
Claus Guttesen <kometen@gmail.com>
2020-04-12 00:48:56 +0200

    Initial commit.

---
+++ src/main.rs
@@ -0,0 +1,44 @@
+extern crate xml;
+
+use std::fs::File;
+use std::io::BufReader;
+
+use xml::reader::{EventReader, XmlEvent};
+
+fn indent(size: usize) -> String {
+    const INDENT: &'static str = "    ";
+    (0..size).map(|_| INDENT)
+             .fold(String::with_capacity(size*INDENT.len()), |r, s| r + s)
+}
+
+fn main() {
+    let file = File::open("output_vegvesen.xml").unwrap();
+    let file = BufReader::new(file);
+
+    let parser = EventReader::new(file);
+    let mut depth = 0;
+    for e in parser {
+        match e {
+            Ok(XmlEvent::StartElement { name, attributes, .. }) => {
+                print!("{}+{}", indent(depth), name);
+                for attribute in attributes {
+                    print!(":{}", attribute);
+                }
+                println!();
+                depth += 1;
+            }
+            Ok(XmlEvent::Characters(s)) => {
+                println!("{}{}", indent(depth), s);
+            }
+            Ok(XmlEvent::EndElement { name }) => {
+                depth -= 1;
+                println!("{}-{}", indent(depth), name);
+            }
+            Err(e) => {
+                println!("Error: {}", e);
+                break;
+            }
+            _ => {}
+        }
+    }
+}
